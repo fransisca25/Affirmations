e5234a979227219dd0b18512b0af692babc6274f
fransisca25 <fcel2510@gmail.com>
2022-05-19 13:52:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.GridView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //To initialize data
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        //To improve performance
+        recyclerView.setHasFixedSize(true)
+    }
+}
+
